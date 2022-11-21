80e18c11cbdcbc79f2f7510a0957460de339a24f
Ivan Nikolov <nikolov.iv@gmail.com>
2022-11-21 15:05:59 +0200

    Added pipeline

--- src/jobs/Test.groovy
+++ src/jobs/Test.groovy
@@ -1,5 +1,14 @@
-mavenJob('Test Jenkins Job') {
-    description 'Testing '
+pipeline {
+    agent any
 
-
+    stages{
+        stage(PullAndRun) {
+            steps {
+                git changelog: false, credentialsId: 'ServiceUser', url: 'https://github.com/nikolov-ivan/Jenkins.git'
+                withMaven(globalMavenSettingsConfig: 'maven-global-settings', jdk: 'Java SE Development Kit 11', maven: 'Maven 3.6.3'){
+                    sh 'mvn clean test -U -Dsurefire.suiteXmlFiles= src/test/java/xml/${xmlToRun -Denvironment=${environ}'
+                }
+            }
+        }
+    }
 }
