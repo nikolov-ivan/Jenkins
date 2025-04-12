5fb64f864946e1f40121e4da22f94b1d3bafaee7
Ivan Nikolov <nikolov.iv@gmail.com>
2025-04-12 07:10:18 +0300

    commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,10 +12,9 @@ public class FirstTest {
         try {
             // Създаване на желания Chrome капацитет
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.setCapability("browserVersion", "100");
-            chromeOptions.setCapability("platformName", "Windows");
+
 // Showing a test name instead of the session id in the Grid UI
-            chromeOptions.setCapability("se:name", "My simple test");
+
 // Other type of metadata can be seen in the Grid UI by clicking on the
 // session info or via GraphQL
             chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value");
@@ -27,4 +27,12 @@ public class FirstTest {
         }
 
     }
+
+//    @Test
+//    public void test2() {
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("http://www.google.com");
+//
+//        driver.quit();
+//    }
 }
