f4395f977127c207987496baf42a811c800ec65d
Ivan Nikolov <nikolov.iv@gmail.com>
2025-04-12 06:52:12 +0300

    commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,5 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -10,21 +10,18 @@ public class FirstTest {
     public void test() {
         try {
             // Създаване на желания Chrome капацитет
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("platform", "LINUX");
-            capabilities.setCapability("version", "latest");
-
-            // URL на Hub-а
-            URL gridUrl = new URL("http://localhost:4444/wd/hub");
-
-            // Създаване на WebDriver с отдалечено свързване към Hub-а
-            WebDriver driver = new RemoteWebDriver(gridUrl, capabilities);
-
-            // Примерен тест
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setCapability("browserVersion", "100");
+            chromeOptions.setCapability("platformName", "Windows");
+// Showing a test name instead of the session id in the Grid UI
+            chromeOptions.setCapability("se:name", "My simple test");
+// Other type of metadata can be seen in the Grid UI by clicking on the
+// session info or via GraphQL
+            chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value");
+            WebDriver driver = new RemoteWebDriver(new URL("http://gridUrl:4444"), chromeOptions);
             driver.get("http://www.google.com");
-            System.out.println("Page Title is: " + driver.getTitle());
-
             driver.quit();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
