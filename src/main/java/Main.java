bf158854602b04303cdd1ee8351c07b56645f552
Ivan Nikolov <nikolov.iv@gmail.com>
2025-04-12 06:37:34 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        try {
+            // Създаване на желания Chrome капацитет
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("platform", "LINUX");
+            capabilities.setCapability("version", "latest");
+
+            // URL на Hub-а
+            URL gridUrl = new URL("http://localhost:4444/wd/hub");
+
+            // Създаване на WebDriver с отдалечено свързване към Hub-а
+            WebDriver driver = new RemoteWebDriver(gridUrl, capabilities);
+
+            // Примерен тест
+            driver.get("http://www.google.com");
+            System.out.println("Page Title is: " + driver.getTitle());
+
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
